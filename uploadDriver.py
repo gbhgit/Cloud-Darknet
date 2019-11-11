618eac5ace1424571e53e75b67653f8ca1713574
GabrielHolanda <gabrielbandeira@lapisco.ifce.edu.br>
2019-11-11 17:04:39 -0300

    update uploadDriver

--- uploadDriver.py
+++ uploadDriver.py
@@ -4,6 +4,8 @@ from pydrive.auth import GoogleAuth
 from google.colab import auth
 from oauth2client.client import GoogleCredentials
 
+import time
+import datetime
 
 def export_folder(filename, out_drive_folder_id):
   '''Export colab folder to drive folder '''
@@ -11,16 +13,21 @@ def export_folder(filename, out_drive_folder_id):
                                         "id": out_drive_folder_id}]})
   # Zip folder
   #!echo '/content/sample_data'
-  cmd = "zip -r out.zip " + filename 
+  new_name = get_filename()
+  cmd = "zip -r out-{}.zip ".format(new_name) + filename 
   returned_value = os.system(cmd)
   print('returned value:', returned_value)
   
   # Export file
-  file.SetContentFile('out.zip')#
+  file.SetContentFile('out-{}.zip'.format(new_name))#
   file.Upload() 
   #!rm out.zip
   return True
 
+def get_filename():
+  ts = time.time()
+  st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d-%H-%M')
+  return st
 
 
 # Authenticate and create the PyDrive client.
@@ -31,5 +38,6 @@ gauth.credentials = GoogleCredentials.get_application_default()
 drive = GoogleDrive(gauth)
 
 weights_name = '/content/darknet/backup'
-out_drive_folder_id = '1LvWCZ5hX7xjnRV0_p9w22f4zsg12Qzxx'
+#out_drive_folder_id = '1LvWCZ5hX7xjnRV0_p9w22f4zsg12Qzxx'
+out_drive_folder_id = os.environ.get('OUTPUT_FOLDER_ID')
 export_folder(weights_name, out_drive_folder_id)
